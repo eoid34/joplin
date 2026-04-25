1e9523179aeee8edcaf45b3cd3590a253bc85296
eoid34 <277161863+eoid34@users.noreply.github.com>
2026-04-25 19:27:46 +0800

    Change profileDir to use template literals
    Refactor profileDir assignment to use template literals.

--- packages/lib/determineBaseAppDirs.ts
+++ packages/lib/determineBaseAppDirs.ts
@@ -1,15 +1,15 @@
 import { homedir } from 'os';
 import { toSystemSlashes } from './path-utils';
-import { dirname, join } from 'path';
+import { dirname } from 'path';
 
 export default (profileFromArgs: string, appName: string, altInstanceId: string) => {
 	let homeDir = dirname(process.execPath);
 	let profileDir = profileFromArgs;
 
 	if (!altInstanceId) {
-		profileDir = join(homeDir, 'data', 'profile');
+		profileDir = `${homeDir}/data/profile`;
 	} else {
-		profileDir = join(homeDir, 'data', 'altprofile', `${appName}-${altInstanceId}`);
+		profileDir = `${homeDir}/data/altprofile/${appName}-${altInstanceId}`;
 	}
 
 	return {
